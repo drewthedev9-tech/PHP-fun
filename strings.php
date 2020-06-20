a04bf69afb13b2f4814b299f49816b8bf2e203f3
drewthedev9@gmail.com <45505005+drewthedev9-tech@users.noreply.github.com>
2020-06-19 20:26:30 -0400

    first commit

---
+++ strings.php
@@ -0,0 +1,62 @@
+<?php
+
+// strings
+$stringOne ='my email is :';
+$stringTwo = 'drewthedev@gmail.com';
+
+// concatinationg the strings.
+// echo $stringOne.$stringTwo;
+
+$name = 'Mario';
+// echo 'Hey my name is' . $name;
+
+// variable interpolatin - [utting variables directly into string
+// use  "" double quotes
+// echo "hey my name is $name";
+
+// include special charecters
+// echo "the ninja screamed \"whaaaa\"";
+// echo 'The ninja screamed "whaaaa"';
+
+
+// will treat the variable above like an array.
+// echo $name[1];
+
+// out of the box function built into PHP.
+// test the length of whatever in the perntheses
+// echo strlen($name);
+// echo strtoupper($name);
+// echo strtolower($name);
+// built in functions.
+
+echo str_replace('M','w',$name);
+
+// Numbers Tutorial 6///////
+
+// Basic operators 
+
+$radius = 25;
+$pi = 3.14;
+
+echo $pi * $radius ^2;
+
+// Tutorial 7 - arrays
+
+$peopleOne = []
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  
+   <title>First PHP file</title>
+</head>
+<body>
+   <!-- <div><?php echo $name;?></div> -->
+  
+   
+</body>
+</html>
